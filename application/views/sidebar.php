c10b4d33afd419c7dbe8874c79975d69fc9633ad
Alfin Auzikri <iyalfin@gmail.com>
2022-01-09 03:31:59 +0700

    sepertinya final

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -3,7 +3,7 @@
     <li class="sidebar-title">
       Main
     </li>
-    <li>
+    <li class="active-page">
       <a href="<?= base_url() ?>"><i data-feather="home"></i>Dashboard</a>
     </li>
     <li class="sidebar-title">
@@ -12,20 +12,5 @@
     <li>
       <a href="<?= base_url('dwh/prediksi') ?>"><i data-feather="edit"></i>Prediksi Penjualan</a>
     </li>
-    <!-- <li class="sidebar-title">
-      Data
-    </li>
-    <li>
-      <a href="email.html"><i data-feather="list"></i>Daftar Fakta</a>
-    </li>
-    <li>
-      <a href="email.html"><i data-feather="list"></i>Daftar Customer</a>
-    </li>
-    <li>
-      <a href="email.html"><i data-feather="list"></i>Daftar Film</a>
-    </li>
-    <li>
-      <a href="email.html"><i data-feather="list"></i>Daftar Staf</a>
-    </li> -->
   </ul>
 </div>
